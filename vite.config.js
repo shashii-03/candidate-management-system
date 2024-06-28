1fefefd6697977a0d0531013dd6d0615b41d5030
shashii-03 <shashiiii3901@gmail.com>
2024-06-28 15:24:39 +0530

    fixed cors issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5173'
+    }
+  },
   plugins: [react()],
+  base: "https://github.com/shashii-03/candidate-management-system.git",
 })
